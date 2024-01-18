593803fabfadb710a7b31f6de6a40ae5352bb856
Alex Nutt <axn043@student.bham.ac.uk>
2024-01-18 19:24:52 +0000

    add grammar to support longer and shorter test cases + add probabilites to new grammar

--- testGeneration_Grammar.py
+++ testGeneration_Grammar.py
@@ -1,7 +1,7 @@
 # This file implements a simple BNF grammar for the structure of single frame UDS requests using a dictionary that maps nonterminal symbols to their corresponding expansions
 import re
-import random
 from textwrap import wrap
+import numpy
 UDS_Grammar: "dict[str, list[str]]" =  {
     "<start>": ["<udsReq>"],
     "<udsReq>": ["<SID><subfunction><data-2>", "<SID><data-1>"],
@@ -13,6 +13,17 @@ UDS_Grammar: "dict[str, list[str]]" =  {
     "<hex>": ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
     "<hex-2>" : ["0","1","2","3","4","5","6","7"]
 }
+
+UDS_Grammar2: "dict[str,list[str]]" = { # more exhustive version (allows for data to be less than 8 bytes but also more (requiring more than 1 CAN frame to transmit))
+    "<start>": {"options":["<udsReq>"],"probs":[1]}, # only one option
+    "<udsReq>": {"options":["<SID><subfunction><data>", "<SID><data>"],"probs":[0.5,0.5]}, # equal chances
+    "<SID>": {"options":["10","11","27","28","29","3e","83","84","85","86","87","22","24","2a","2c","2e","3d","14","19","2f","31","34","35","36","37","38"],"probs":[0.038461538 for x in range(26)]}, # currently equal chances, will be updated based on results from service scanner
+    "<subfunction>": {"options":["<hex-2><hex>"],"probs":[1]},
+    "<data>": {"options":["<byte><byte>", "<data><byte>"], "probs":[0.04,0.96]},
+    "<byte>": {"options":["<hex><hex>"],"probs": [1]}, # only 1 option
+    "<hex>": {"options":["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],"probs":[0.0625 for x in range(16)]}, # equal chances
+    "<hex-2>" : {"options":["0","1","2","3","4","5","6","7"],"probs":[0.125 for x in range(8)]} #equal chances
+    }
 startTerm = "<start>"
 nonTerminalPattern = '<[^<> ]+>'
 
@@ -22,15 +33,34 @@ def generateInput():
 
     while(len(re.findall(nonTerminalPattern,currentTerm)) !=0): # while there are still non terminal symbols to expand
         # print(re.findall(nonTerminalPattern,currentTerm))
+        # print(currentTerm)
+
         expandingSymbol: str = re.findall(nonTerminalPattern,currentTerm)[0] # get first non terminal symbol
-        expansionList: "list[str]" = UDS_Grammar[expandingSymbol] # get list of expanding options for that symbol
-        chosenExpansion : str = random.choice(expansionList) # pick one of the options randomly
+        expansionOptions: "list[str]" = UDS_Grammar2[expandingSymbol] # get list of expanding options for that symbol
+        print(expansionOptions)
+        probabilites = expansionOptions["probs"]
+        expansionList = expansionOptions["options"]
+        print(expansionList)
+        print(probabilites)
+        if (len(expansionList) == 1):
+            chosenExpansion = expansionList[0]
+        else:
+            chosenExpansion = numpy.random.choice(expansionList,p=probabilites) # choose an expansion based off the probabilites
+
+        # print("chosen = "+chosenExpansion)
+
         # print(expandingSymbol + " ----> "+ chosenExpansion)
         currentTerm = currentTerm.replace(expandingSymbol,chosenExpansion,1) # replace the non terminal symbol with the chosen expansion
+        
 
     byteList : "list[str]" = wrap(currentTerm,2) # split generated string into list of hex strings for each byte
     generatedInput : list[int] = list(map(lambda hex: int(hex,16),byteList))
 
     return generatedInput
 
-print(generateInput())
+y = []
+for x in range(500):
+    y.append(len(generateInput()))
+print("Average len = "+str(sum(y)/len(y)))
+print("Max input len = "+str(max(y)))
+print("Min input len = "+str(min(y)))
