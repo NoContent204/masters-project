4e0bc2be303e8f92343d37aa8b6a47526312af05
Alex Nutt <axn043@student.bham.ac.uk>
2024-02-03 14:53:02 +0000

    slight tidy up + only return 7 bytes of generated input (UDS reqs are only a single frame, duh)

--- testGeneration_Grammar.py
+++ testGeneration_Grammar.py
@@ -14,12 +14,12 @@ UDS_Grammar: "dict[str, list[str]]" =  {
     "<hex-2>" : ["0","1","2","3","4","5","6","7"]
 }
 
-UDS_Grammar2: "dict[str,list[str]]" = { # more exhustive version (allows for data to be less than 8 bytes but also more (requiring more than 1 CAN frame to transmit))
+UDS_Grammar2: "dict[str,dict[str,list[any]]]" = { # grammar using probabilities, allows for varying length of the data
     "<start>": {"options":["<udsReq>"],"probs":[1]}, # only one option
     "<udsReq>": {"options":["<SID><subfunction><data>", "<SID><data>"],"probs":[0.5,0.5]}, # equal chances
     "<SID>": {"options":["10","11","27","28","29","3e","83","84","85","86","87","22","24","2a","2c","2e","3d","14","19","2f","31","34","35","36","37","38"],"probs":[0.038461538 for x in range(26)]}, # currently equal chances, will be updated based on results from service scanner
     "<subfunction>": {"options":["<hex-2><hex>"],"probs":[1]},
-    "<data>": {"options":["<byte><byte>", "<data><byte>"], "probs":[0.04,0.96]},
+    "<data>": {"options":["<byte><byte>", "<data><byte>"], "probs":[0.5,0.5]},
     "<byte>": {"options":["<hex><hex>"],"probs": [1]}, # only 1 option
     "<hex>": {"options":["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],"probs":[0.0625 for x in range(16)]}, # equal chances
     "<hex-2>" : {"options":["0","1","2","3","4","5","6","7"],"probs":[0.125 for x in range(8)]} #equal chances
@@ -37,30 +37,31 @@ def generateInput():
 
         expandingSymbol: str = re.findall(nonTerminalPattern,currentTerm)[0] # get first non terminal symbol
         expansionOptions: "list[str]" = UDS_Grammar2[expandingSymbol] # get list of expanding options for that symbol
-        print(expansionOptions)
+        #print(expansionOptions)
+
         probabilites = expansionOptions["probs"]
         expansionList = expansionOptions["options"]
-        print(expansionList)
-        print(probabilites)
-        if (len(expansionList) == 1):
-            chosenExpansion = expansionList[0]
-        else:
-            chosenExpansion = numpy.random.choice(expansionList,p=probabilites) # choose an expansion based off the probabilites
+        chosenExpansion = numpy.random.choice(expansionList,p=probabilites) # choose an expansion based off the probabilites
+
+        #print(chosenExpansion)
 
         # print("chosen = "+chosenExpansion)
 
-        # print(expandingSymbol + " ----> "+ chosenExpansion)
+        #print(expandingSymbol + " ----> "+ chosenExpansion)
         currentTerm = currentTerm.replace(expandingSymbol,chosenExpansion,1) # replace the non terminal symbol with the chosen expansion
         
 
     byteList : "list[str]" = wrap(currentTerm,2) # split generated string into list of hex strings for each byte
     generatedInput : list[int] = list(map(lambda hex: int(hex,16),byteList))
-
-    return generatedInput
+    if (len(generatedInput) > 7):
+        return generatedInput[:7] # return only 7 bytes
+    else:
+        return generatedInput
 
 y = []
-for x in range(500):
-    y.append(len(generateInput()))
-print("Average len = "+str(sum(y)/len(y)))
-print("Max input len = "+str(max(y)))
-print("Min input len = "+str(min(y)))
+#print(generateInput())
+# for x in range(1000):
+#     y.append(len(generateInput()))
+# print("Average len = "+str(sum(y)/len(y)))
+# print("Max input len = "+str(max(y)))
+# print("Min input len = "+str(min(y)))
