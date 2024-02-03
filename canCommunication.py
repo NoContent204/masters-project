d45e250d949b6a1e9fe842fcebd3f03b9e5d6d7f
Alex Nutt <axn043@student.bham.ac.uk>
2024-02-03 16:11:16 +0000

    create canCommunication.py file for common functions used for the can bus

---
+++ canCommunication.py
@@ -0,0 +1,21 @@
+# File for functions relating to communicating via the CAN bus
+import can
+import numpy as np
+
+TX_CAN_ID = 0x7df
+RX_CAN_ID = 0x72e
+bus = can.Bus(interface='socketcan' , channel='can0', bitrate=500000) # set up can bus communication
+bus.set_filters([{"can_id": RX_CAN_ID, "can_mask": RX_CAN_ID, "extended": False}]) # set filter to only get responses from the response CAN-ID for UDS
+
+
+def sendUDSReq(dataFrame): # SID included within dataFrame array
+    PCI = [len(dataFrame)] # PCI (CAN-TP) header includes length of the data 
+    canData: list[int] = PCI + dataFrame # combine the two into a single array
+    canData = list(np.pad(canData,(0,8-len(canData)),'constant',constant_values=0)) # padd data with zeros
+    bus.send(can.Message(arbitration_id=TX_CAN_ID, data=canData, is_extended_id=False)) # send data
+    resp = bus.recv(timeout=6) # get response
+    return resp
+
+
+def sendWakeUpMessage(): # function to send a wake up message to the ECU (Tester present) to make sure ECU will respond to fuzzing messages
+    bus.send(can.Message(arbitration_id=TX_CAN_ID, data=[0x02,0x3e,0x00,0x00,0x00,0x00,0x00,0x00], is_extended_id=False))
