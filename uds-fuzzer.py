3579bf57ac3d2a9e618dac2a687a26f08d0bb04d
Alex Nutt <axn043@student.bham.ac.uk>
2024-02-06 11:43:40 +0000

    add actual fuzzing functionality + generating random CAN frames

--- uds-fuzzer.py
+++ uds-fuzzer.py
@@ -2,12 +2,24 @@ import canCommunication
 from testGeneration_Grammar import generateInput
 from testGeneration_Mutation import bitFlipping, byteShift, logicalMutations
 from ECUFeedback import readDTCInformation, responseTiming, recordNonUDSTraffic, getAverageResponseTime
+from availableUDSServices import SIDs
 import argparse
+import random
+
+def randCANframe():
+    frame = []
+    dlc = random.randint(1,6) # exlcude SID
+    SID = random.choice(SIDs)["SID"] # randomly pick an SID
+    frame.append(SID)
+    for _ in range(dlc):
+        frame.append(random.getrandbits(8)) # generate random data
+    return frame
 
 def main():
     testGenerationMethods = []
     feedbackMethods = []
     avgRespT = None
+    totalDTCs = 0
 
     parser = argparse.ArgumentParser(description = "Fuzz the UDS protocol on an ECU")
     parser.add_argument("-m", "--mutational", action='store_true', help="Enable the use of mutations on inputs") # enable mutations on inputs
@@ -33,10 +45,39 @@ def main():
         avgRespT = getAverageResponseTime()
         feedbackMethods.append(responseTiming)
     if (args.traffic):
+        recordNonUDSTraffic(True)
         feedbackMethods.append(recordNonUDSTraffic)
 
     if (len(feedbackMethods) == 0): # no feedback methods given
         print("Please provide at least one method for feedback (-d, -ti or -tr). Use -h or --help for help")
         exit(0)
+
+    while True:
+        if (generateInput in testGenerationMethods):
+            initialData = generateInput()
+        else:
+            initialData = randCANframe()
+        
+        if (args.mutational):
+            mutate = random.choice([method for method in testGenerationMethods if method != generateInput]) # pick a random mutation function
+            data = mutate(initialData, args.mutation_iterations)            
+        else:
+            data = initialData
+        
+        if (args.timing):
+            x = responseTiming(avgT=avgRespT, data=data, timeThresholdMultipler=args.timing)
+            if (x[0]): # response took long enough to trigger threshold
+                print(str(data) + " casued ECU to take " + str(x[1]) + "seconds to respond") # CHANGE TO WRITE TO FILE LATER (with reason)
+        else:
+            resp = canCommunication.sendUDSReq(data)
+        if (args.dtc):
+            dtcs = readDTCInformation()
+            if (dtcs > totalDTCs): # test caused DTC(s)
+                print(str(data) + " caused " + str(dtcs - totalDTCs) + "DTC(s)") # CHANGE TO WRITE TO FILE LATER (with reason)
+            totalDTCs = dtcs
+        if (args.traffic):
+            newTraffic = recordNonUDSTraffic(False)
+            if (bool(newTraffic)): # test caused new CAN traffic
+                print(str(data) + " caused new CAN traffic to occur: " + str(newTraffic)) # CHANGE TO WRITE TO FILE LATER (with reason)
 
 main()
