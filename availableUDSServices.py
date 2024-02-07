40ef48a1deda2f43efe0006b47d3cf559ed38dfe
Alex Nutt <axn043@student.bham.ac.uk>
2024-02-07 13:39:43 +0000

    put code into a main function to stop it from being executed when imported

--- availableUDSServices.py
+++ availableUDSServices.py
@@ -161,78 +161,78 @@ SIDs = [
 #print(bus.filters)
 #bus.send(message) # send a message to "wake up" the ECU    
 #tx_canID = 0x7df
+def main():
+   canCommunication.sendWakeUpMessage()
+   availableServices = list(SIDs)
 
-canCommunication.sendWakeUpMessage()
-availableServices = list(SIDs)
-
-for service in SIDs:
-    SID = service["SID"]
-    print("Scanning SID: "+ hex(SID))
-    for x in range(5): # try to get a response 5 times to avoid getting None 
-      #resp = sendUDSReq(SID, []) # start with blank data
-      resp = canCommunication.sendUDSReq([SID] + [])
-      if (resp != None and resp.arbitration_id != 0x72e):
-         #resp = sendUDSReq(SID, []) # sometimes we get a random message form id 4? so we have to resend 
+   for service in SIDs:
+      SID = service["SID"]
+      print("Scanning SID: "+ hex(SID))
+      for x in range(5): # try to get a response 5 times to avoid getting None 
+         #resp = sendUDSReq(SID, []) # start with blank data
          resp = canCommunication.sendUDSReq([SID] + [])
-      if (resp == None):
-         #resp = sendUDSReq(SID, [0x01]) or sendUDSReq(SID,[0x11,0x11,0x11,0x11,0x11,0x11]) # try either a subfunction of 0x01 or all the data being 1's
-         resp = canCommunication.sendUDSReq([SID] + [0x01]) or canCommunication.sendUDSReq([SID] + [0x11,0x11,0x11,0x11,0x11,0x11])
+         if (resp != None and resp.arbitration_id != 0x72e):
+            #resp = sendUDSReq(SID, []) # sometimes we get a random message form id 4? so we have to resend 
+            resp = canCommunication.sendUDSReq([SID] + [])
+         if (resp == None):
+            #resp = sendUDSReq(SID, [0x01]) or sendUDSReq(SID,[0x11,0x11,0x11,0x11,0x11,0x11]) # try either a subfunction of 0x01 or all the data being 1's
+            resp = canCommunication.sendUDSReq([SID] + [0x01]) or canCommunication.sendUDSReq([SID] + [0x11,0x11,0x11,0x11,0x11,0x11])
 
 
-      if (resp != None and resp.arbitration_id == 0x72e):
-         break # if we got a response then break the loop
+         if (resp != None and resp.arbitration_id == 0x72e):
+            break # if we got a response then break the loop
 
-    if (resp.data[1] == SID + 0x40): # Postive respose is always the services id + 0x40 e.g. positive reponse for 0x10 is 0x50 
-      print("Postive response - Service available")
-    elif (resp.data[1] == 0x7f): # Negative response
+      if (resp.data[1] == SID + 0x40): # Postive respose is always the services id + 0x40 e.g. positive reponse for 0x10 is 0x50 
+         print("Postive response - Service available")
+      elif (resp.data[1] == 0x7f): # Negative response
 
-      # All the response codes (other than 0x11) imply that the service is supported but a specific error has occured, we can still fuzz these services
-      if (resp.data[3] == 0x11): # Specific negative response codes
-         print("Service not supported")
-         #availableServices.remove(SID) 
-         availableServices = [service for service in availableServices if not (service['SID'] == SID)]# service is not supported by the BCM so remove the service ID from the list
-      elif (resp.data[3] == 0x12):
-         print(hex(SID) + " - Sub-function not supported")
-      elif (resp.data[3] == 0x13):
-         print(hex(SID) + " - Invalid message length/format")
-      elif (resp.data[3] == 0x14):
-         print(hex(SID) + " - Response too long")
-      elif (resp.data[3] == 0x21):
-         print(hex(SID) + " - Busy- repeat request")
-      elif (resp.data[3] == 0x22):
-         print(hex(SID) + " - Conditions not correct")
-      elif (resp.data[3] == 0x24):
-         print(hex(SID) + " - Request sequence error")
-      elif (resp.data[3] == 0x25):
-         print(hex(SID) + " - No response from subnet component")
-      elif (resp.data[3] == 0x26):
-         print(hex(SID) + " - Failure prevents execution of requested action")
-      elif (resp.data[3] == 0x31):
-         print(hex(SID) + " - Request out of range")
-      elif (resp.data[3] == 0x33):
-         print(hex(SID) + " - Security access denied")
-      elif (resp.data[3] == 0x35):
-         print(hex(SID) + " - Invalid key")
-      elif (resp.data[3] == 0x36):
-         print(hex(SID) + " - Exceeded number of attempts")
-      elif (resp.data[3] == 0x37):
-         print(hex(SID) + " - Required time delay has not expired")
-      elif (resp.data[3] == 0x70):
-         print(hex(SID) + " - Upload/dowload not accepted")
-      elif (resp.data[3] == 0x71):
-         print(hex(SID) + " - Transfer data suspended")
-      elif (resp.data[3] == 0x72):
-         print(hex(SID) + " - Programming failure")
-      elif (resp.data[3] == 0x73):
-         print(hex(SID) + " - Wrong block sequence counter")
-      elif (resp.data[3] == 0x78):
-         print(hex(SID) + " - Request received - repsonse pending")
-      elif (resp.data[3] == 0x7e):
-         print(hex(SID) + " - Sub funtion not support in active session")
-      elif (resp.data[3] == 0x7f):
-         print(hex(SID) + " - Service not supported in active session")
-      else:
-         print("Unexpected issue error code "+ hex(resp.data[3]))
-with open('availableServices.log',"a") as log: # Save list of avaiable services to file (to be used by main fuzzer)
-   for service in availableServices:
-      print(service,file=log)
+         # All the response codes (other than 0x11) imply that the service is supported but a specific error has occured, we can still fuzz these services
+         if (resp.data[3] == 0x11): # Specific negative response codes
+            print("Service not supported")
+            #availableServices.remove(SID) 
+            availableServices = [service for service in availableServices if not (service['SID'] == SID)]# service is not supported by the BCM so remove the service ID from the list
+         elif (resp.data[3] == 0x12):
+            print(hex(SID) + " - Sub-function not supported")
+         elif (resp.data[3] == 0x13):
+            print(hex(SID) + " - Invalid message length/format")
+         elif (resp.data[3] == 0x14):
+            print(hex(SID) + " - Response too long")
+         elif (resp.data[3] == 0x21):
+            print(hex(SID) + " - Busy- repeat request")
+         elif (resp.data[3] == 0x22):
+            print(hex(SID) + " - Conditions not correct")
+         elif (resp.data[3] == 0x24):
+            print(hex(SID) + " - Request sequence error")
+         elif (resp.data[3] == 0x25):
+            print(hex(SID) + " - No response from subnet component")
+         elif (resp.data[3] == 0x26):
+            print(hex(SID) + " - Failure prevents execution of requested action")
+         elif (resp.data[3] == 0x31):
+            print(hex(SID) + " - Request out of range")
+         elif (resp.data[3] == 0x33):
+            print(hex(SID) + " - Security access denied")
+         elif (resp.data[3] == 0x35):
+            print(hex(SID) + " - Invalid key")
+         elif (resp.data[3] == 0x36):
+            print(hex(SID) + " - Exceeded number of attempts")
+         elif (resp.data[3] == 0x37):
+            print(hex(SID) + " - Required time delay has not expired")
+         elif (resp.data[3] == 0x70):
+            print(hex(SID) + " - Upload/dowload not accepted")
+         elif (resp.data[3] == 0x71):
+            print(hex(SID) + " - Transfer data suspended")
+         elif (resp.data[3] == 0x72):
+            print(hex(SID) + " - Programming failure")
+         elif (resp.data[3] == 0x73):
+            print(hex(SID) + " - Wrong block sequence counter")
+         elif (resp.data[3] == 0x78):
+            print(hex(SID) + " - Request received - repsonse pending")
+         elif (resp.data[3] == 0x7e):
+            print(hex(SID) + " - Sub funtion not support in active session")
+         elif (resp.data[3] == 0x7f):
+            print(hex(SID) + " - Service not supported in active session")
+         else:
+            print("Unexpected issue error code "+ hex(resp.data[3]))
+   with open('availableServices.log',"a") as log: # Save list of avaiable services to file (to be used by main fuzzer)
+      for service in availableServices:
+         print(service,file=log)
