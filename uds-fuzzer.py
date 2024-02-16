bca4703524af9b3fbeb002ad6d3eb8379bb6a22f
Alex Nutt <axn043@student.bham.ac.uk>
2024-02-16 16:12:13 +0000

    change to recording to file, add check for response for dtcs, add radamsa lib support

--- uds-fuzzer.py
+++ uds-fuzzer.py
@@ -5,6 +5,9 @@ from ECUFeedback import readDTCInformation, responseTiming, recordNonUDSTraffic,
 from availableUDSServices import SIDs
 import argparse
 import random
+import pyradamsa
+
+crashFile = "crashes.txt"
 
 def randCANframe():
     frame = []
@@ -16,6 +19,7 @@ def randCANframe():
     return frame
 
 def main():
+    radamsa = pyradamsa.Radamsa()
     testGenerationMethods = []
     feedbackMethods = []
     avgRespT = None
@@ -26,10 +30,16 @@ def main():
     parser.add_argument("-mi", "--mutation-iterations", type=int, default=10, help="Set number of times an input is mutated (default is 10)") # set number if iterations for mutations
     parser.add_argument("-g", "--grammar", action='store_true', help="Enable the use of inputs produced from a BNF grammar") # enable inputs from grammar
     parser.add_argument("-d", "--dtc", action="store_true", help="Enable the use of DTC for feedback") # enable feedback via DTC
-    parser.add_argument("-ti", "--timing", type=int, help="Enable the use of response timings for feedback, provide threshold multipler for timings") # enable feedback via repsonse timings
+    parser.add_argument("-ti", "--timing", type=int, help="Enable the use of response timings for feedback, provide threshold multipler for timings. Note: this will slow down the performance as the fuzzer will wait for the timeout before going to the next test case") # enable feedback via repsonse timings
     parser.add_argument("-tr", "--traffic", action="store_true", help="Enable the use of the CAN traffic for feedback") # enable feedback via CAN traffic
+    parser.add_argument("-r", "--radamsa",action="store_true", help="Enable the use of radamsa for fuzz testcase generation") # enable radamsa use
     args = parser.parse_args()
     
+    if (args.mutational and args.radamsa):
+        print("Please use either the muatational setting OR the radamsa setting or neither")
+        exit(0)
+    if (args.radamsa):
+        testGenerationMethods.append(radamsa.fuzz)
     if (args.mutational):
         testGenerationMethods.append(byteShift)
         testGenerationMethods.append(bitFlipping)
@@ -37,12 +47,12 @@ def main():
     if (args.grammar):
         testGenerationMethods.append(generateInput)
     if (len(testGenerationMethods) == 0): # no test generation methods given
-        print("Please provide at least one method for generating inputs (-m or -g). Use -h or --help for help")
+        print("Please provide at least one method for generating inputs (-m, -g or -r). Use -h or --help for help")
         exit(0)
     if (args.dtc):
         feedbackMethods.append(readDTCInformation)
     if (args.timing):
-        avgRespT = getAverageResponseTime()
+        avgRespT = getAverageResponseTime(5)
         feedbackMethods.append(responseTiming)
     if (args.traffic):
         recordNonUDSTraffic(True)
@@ -52,36 +62,53 @@ def main():
         print("Please provide at least one method for feedback (-d, -ti or -tr). Use -h or --help for help")
         exit(0)
 
+    logFile = open(crashFile,'a')
+
     while True:
-        if (generateInput in testGenerationMethods):
-            initialData = generateInput()
-        else:
-            initialData = randCANframe()
-        
-        if (args.mutational):
-            mutate = random.choice([method for method in testGenerationMethods if method != generateInput]) # pick a random mutation function
-            data = mutate(initialData, args.mutation_iterations)            
-        else:
-            data = initialData
-        
-        if (args.timing):
-            x = responseTiming(avgT=avgRespT, data=data, timeThresholdMultipler=args.timing)
-            if (x[0]): # response took long enough to trigger threshold
-                print(bytes(data).hex() + " casued ECU to take " + str(x[1]) + "seconds to respond") # CHANGE TO WRITE TO FILE LATER (with reason)
-        else:
-            resp = canCommunication.sendUDSReq(data)
-        
-        if (resp == None):
-            print(bytes(data).hex() + " caused a None reponse (likely due to timeout)") # CHANGE TO WRITE TO FILE LATER (with reason)
-        
-        if (args.dtc):
-            dtcs = readDTCInformation()
-            if (dtcs > totalDTCs): # test caused DTC(s)
-                print(bytes(data).hex() + " caused " + str(dtcs - totalDTCs) + "DTC(s)") # CHANGE TO WRITE TO FILE LATER (with reason)
-            totalDTCs = dtcs
-        if (args.traffic):
-            Traffic = recordNonUDSTraffic(False)
-            if (bool(Traffic[0])): # test caused new CAN traffic
-                print(bytes(data).hex() + " caused new CAN traffic to occur: " + str(Traffic[0])) # CHANGE TO WRITE TO FILE LATER (with reason) # place holder 
+        try:
+            if (generateInput in testGenerationMethods):
+                initialData = generateInput()
+            else:
+                initialData = randCANframe()
+            
+
+            mutationMethods = [method for method in testGenerationMethods if method != generateInput]
+            if (args.mutational):
+                mutate = random.choice(mutationMethods) # pick a random mutation function
+                data = mutate(initialData, args.mutation_iterations)            
+            elif (args.radamsa):
+                mutate = mutationMethods[0] # if radamsa is set there will only be one muation method
+                data = mutate(initialData,max_mut=7)
+            else:
+                data = initialData
+            
+            if (args.timing):
+                respData = responseTiming(avgT=avgRespT, data=data, timeThresholdMultipler=args.timing)
+                resp = respData[0]
+                if (respData[1]): # response took long enough to trigger threshold
+                    print(bytes(data).hex() + " casued ECU to take " + str(respData[1]) + "seconds to respond\n") # CHANGE TO WRITE TO FILE LATER (with reason)
+            else:
+                resp = canCommunication.sendUDSReq(data)
+            
+            if (resp == None):
+                logFile.write(bytes(data).hex() + " caused a None reponse (likely due to timeout from no response)\n")
+                #print(bytes(data).hex() + " caused a None reponse (likely due to timeout from no response)\n") # CHANGE TO WRITE TO FILE LATER (with reason)
+            
+            if (args.dtc):
+                dtcs = readDTCInformation()
+                if (dtcs != -1): # make sure we got something
+                    if (dtcs > totalDTCs): # test caused DTC(s)
+                        logFile.write(bytes(data).hex() + " caused " + str(dtcs - totalDTCs) + " DTC(s)\n")
+                        #print(bytes(data).hex() + " caused " + str(dtcs - totalDTCs) + " DTC(s)\n") # CHANGE TO WRITE TO FILE LATER (with reason)
+                    totalDTCs = dtcs
+            if (args.traffic):
+                Traffic = recordNonUDSTraffic(False)
+                if (bool(Traffic[0])): # test caused new CAN traffic
+                    logFile.write(bytes(data).hex() + " caused new CAN traffic to occur: " + str(Traffic[0]) +"\n")
+                    #print(bytes(data).hex() + " caused new CAN traffic to occur: " + str(Traffic[0]) +"\n") # CHANGE TO WRITE TO FILE LATER (with reason) # place holder 
+        except KeyboardInterrupt:
+            logFile.close()
+            exit(0)
+
 
 main()
