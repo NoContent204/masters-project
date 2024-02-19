916bfd045262046faabb0695278cdd2d41d4f083
Alex Nutt <axn043@student.bham.ac.uk>
2024-02-19 12:18:41 +0000

    add check for previously used input + add getting list of usable SIDs from file

--- uds-fuzzer.py
+++ uds-fuzzer.py
@@ -2,37 +2,49 @@ import canCommunication
 from testGeneration_Grammar import generateInput
 from testGeneration_Mutation import bitFlipping, byteShift, logicalMutations
 from ECUFeedback import readDTCInformation, responseTiming, recordNonUDSTraffic, getAverageResponseTime
-from availableUDSServices import SIDs
+import availableUDSServices
 import argparse
 import random
 import pyradamsa
+from os.path import exists
 
 crashFile = "crashes.txt"
+SIDs = []
 
 def randCANframe():
     frame = []
     dlc = random.randint(1,6) # exlcude SID
-    SID = random.choice(SIDs)["SID"] # randomly pick an SID
+    SID = random.choice(SIDs)#["SID"] # randomly pick an SID
     frame.append(SID)
     for _ in range(dlc):
         frame.append(random.getrandbits(8)) # generate random data
     return frame
 
 def main():
+    if (not(exists('availableServices.log'))):
+        availableUDSServices.main()
+
+    f = open('availableServices.log','r')
+    SIDs = list(map(lambda x : int(x,16),f.readlines()))
+    f.close()
     radamsa = pyradamsa.Radamsa()
     testGenerationMethods = []
     feedbackMethods = []
     avgRespT = None
     totalDTCs = 0
+    usedInputs = {[]: 1}
 
     parser = argparse.ArgumentParser(description = "Fuzz the UDS protocol on an ECU")
+    # Input generation arguments
     parser.add_argument("-m", "--mutational", action='store_true', help="Enable the use of mutations on inputs") # enable mutations on inputs
-    parser.add_argument("-mi", "--mutation-iterations", type=int, default=10, help="Set number of times an input is mutated (default is 10)") # set number if iterations for mutations
+    parser.add_argument("-mi", "--mutation-iterations", type=int, default=10, help=" Used in conjunction with -m. Sets the number of times an input is mutated (default is 10)") # set number if iterations for mutations
     parser.add_argument("-g", "--grammar", action='store_true', help="Enable the use of inputs produced from a BNF grammar") # enable inputs from grammar
+    parser.add_argument("-r", "--radamsa",action="store_true", help="Enable the use of radamsa for fuzz testcase generation") # enable radamsa use
+
+    # Feedback arguments
     parser.add_argument("-d", "--dtc", action="store_true", help="Enable the use of DTC for feedback") # enable feedback via DTC
     parser.add_argument("-ti", "--timing", type=int, help="Enable the use of response timings for feedback, provide threshold multipler for timings. Note: this will slow down the performance as the fuzzer will wait for the timeout before going to the next test case") # enable feedback via repsonse timings
     parser.add_argument("-tr", "--traffic", action="store_true", help="Enable the use of the CAN traffic for feedback") # enable feedback via CAN traffic
-    parser.add_argument("-r", "--radamsa",action="store_true", help="Enable the use of radamsa for fuzz testcase generation") # enable radamsa use
     args = parser.parse_args()
     
     if (args.mutational and args.radamsa):
@@ -73,20 +85,24 @@ def main():
             
 
             mutationMethods = [method for method in testGenerationMethods if method != generateInput]
-            if (args.mutational):
-                mutate = random.choice(mutationMethods) # pick a random mutation function
-                data = mutate(initialData, args.mutation_iterations)            
-            elif (args.radamsa):
-                mutate = mutationMethods[0] # if radamsa is set there will only be one muation method
-                data = mutate(initialData,max_mut=7)
-            else:
-                data = initialData
+            data = []
+            while data in usedInputs:
+                if (args.mutational):
+                    mutate = random.choice(mutationMethods) # pick a random mutation function
+                    data = mutate(initialData, args.mutation_iterations)            
+                elif (args.radamsa):
+                    mutate = mutationMethods[0] # if radamsa is set there will only be one muation method
+                    data = mutate(initialData,max_mut=7)
+                else:
+                    data = initialData
+            usedInputs[data] = 1 # add data to list of used inputs
             
             if (args.timing):
                 respData = responseTiming(avgT=avgRespT, data=data, timeThresholdMultipler=args.timing)
                 resp = respData[0]
                 if (respData[1]): # response took long enough to trigger threshold
-                    print(bytes(data).hex() + " casued ECU to take " + str(respData[1]) + "seconds to respond\n") # CHANGE TO WRITE TO FILE LATER (with reason)
+                    logFile.write(bytes(data).hex() + " casued ECU to take " + str(respData[1]) + " seconds to respond\n")
+                    #print(bytes(data).hex() + " casued ECU to take " + str(respData[1]) + "seconds to respond\n") # CHANGE TO WRITE TO FILE LATER (with reason)
             else:
                 resp = canCommunication.sendUDSReq(data)
             
