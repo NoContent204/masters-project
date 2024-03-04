176930c021647b44d51eb722f775db0fa6766063
Alex Nutt <axn043@student.bham.ac.uk>
2024-03-04 12:18:19 +0000

    add logging of response types + printing summary at the end

--- uds_fuzzer.py
+++ uds_fuzzer.py
@@ -1,8 +1,9 @@
 import canCommunication
 from testGeneration_Grammar import generateInput
-from testGeneration_Mutation import bitFlipping, byteShift, logicalMutations
+from testGeneration_Mutation import bitFlipping, byteShift, logicalMutations, byteAddition, byteDeletion, mutateData
 from ECUFeedback import readDTCInformation, responseTiming, recordNonUDSTraffic, getAverageResponseTime, sortTraffic
 import availableUDSServices
+
 import argparse
 import random
 import pyradamsa
@@ -39,7 +40,7 @@ def main():
     parser = argparse.ArgumentParser(description = "Fuzz the UDS protocol on an ECU")
     # Input generation arguments
     parser.add_argument("-m", "--mutational", action='store_true', help="Enable the use of mutations on inputs") # enable mutations on inputs
-    parser.add_argument("-mi", "--mutation-iterations", type=int, default=10, help=" Used in conjunction with -m. Sets the number of times an input is mutated (default is 10)") # set number if iterations for mutations
+    parser.add_argument("-mi", "--mutation-iterations", type=int, default=10, help="Used in conjunction with -m. Sets the number of times an input is mutated (default is 10)") # set number if iterations for mutations
     parser.add_argument("-g", "--grammar", action='store_true', help="Enable the use of inputs produced from a BNF grammar") # enable inputs from grammar
     parser.add_argument("-r", "--radamsa",action="store_true", help="Enable the use of radamsa for fuzz testcase generation") # enable radamsa use
 
@@ -81,7 +82,7 @@ def main():
     usedInputs.write(bytes([0]).hex()+"\n")
     usedInputs.seek(0)
 
-    logFile = open(crashFile,'a')
+    logFile = open(crashFile,'a+')
     if (args.traffic):
         thread = threading.Thread(target=recordNonUDSTraffic, args=(False,logFile,))
         thread.daemon = True
@@ -89,6 +90,8 @@ def main():
         thread.start() # Start the recording of traffic asynchronously
     else:
         canCommunication.sendWakeUpMessage()
+    
+    canCommunication.extendedSession() # put us in extended session
     while True:
         try:
             #print("Fuzzing...")
@@ -100,15 +103,14 @@ def main():
 
             mutationMethods = [method for method in testGenerationMethods if method != generateInput]
             data = [0]
-            #print(list(map(lambda x: x.replace("\n",""), usedInputs.readlines())))
             currentInputs = list(map(lambda x: x.replace("\n",""), usedInputs.readlines()))
             while bytes(data).hex() in currentInputs:
                 if (args.mutational):
-                    mutate = random.choice(mutationMethods) # pick a random mutation function
-                    data = mutate(initialData, args.mutation_iterations)            
+                    #mutate = random.choice(mutationMethods) # pick a random mutation function
+                    data = mutateData(initialData, args.mutation_iterations)            
                 elif (args.radamsa):
                     mutate = mutationMethods[0] # if radamsa is set there will only be one muation method
-                    data = initialData[0] + list(mutate(initialData[1:],max_mut=7)) # ignore SID when using radamsa to mutate
+                    data = [initialData[0]] + list(mutate(initialData[1:],max_mut=6)) # ignore SID when using radamsa to mutate
                 else:
                     data = initialData
             print("Fuzzing with "+bytes(data).hex() + "\n")
@@ -123,10 +125,14 @@ def main():
                 resp = respData[0]
                 if (respData[1]): # response took long enough to trigger threshold
                     logFile.write(bytes(data).hex() + " casued ECU to take " + str(respData[1]) + " seconds to respond\n")
-                    #logFile.flush()
-                    #print(bytes(data).hex() + " casued ECU to take " + str(respData[1]) + "seconds to respond\n") # CHANGE TO WRITE TO FILE LATER (with reason)
             else:
                 resp = canCommunication.sendUDSReq(data)
+            if (resp != None):
+                if resp.data[1] != 0x7f:
+                    logFile.write(bytes(data).hex() + " caused postive response: "+resp.data[2:].hex()+"\n")
+                else:
+                    if (resp.data[3] != 0x13): # ignore errors that are about invalid lengths/formats
+                        logFile.write(bytes(data).hex() + " caused negaitve response. NRC: "+hex(resp.data[3])+"\n")
             
             #if (resp == None): Probably not needed since most requests seem to have no response 
                 #logFile.write(bytes(data).hex() + " caused a None reponse (likely due to timeout from no response)\n")
@@ -137,16 +143,13 @@ def main():
                 if (dtcs != -1): # make sure we got something
                     if (dtcs > totalDTCs): # test caused DTC(s)
                         logFile.write(bytes(data).hex() + " caused " + str(dtcs - totalDTCs) + " DTC(s)\n")
-                        #logFile.flush()
-                        #print(bytes(data).hex() + " caused " + str(dtcs - totalDTCs) + " DTC(s)\n") # CHANGE TO WRITE TO FILE LATER (with reason)
                     totalDTCs = dtcs
 
-            #if (args.traffic):
-                #Traffic = recordNonUDSTraffic(False)
-                #if (bool(Traffic[0])): # test caused new CAN traffic
-                    #logFile.write(bytes(data).hex() + " caused new CAN traffic to occur: " + str(Traffic[0]) +"\n")
-                    #print(bytes(data).hex() + " caused new CAN traffic to occur: " + str(Traffic[0]) +"\n") # CHANGE TO WRITE TO FILE LATER (with reason) # place holder 
         except KeyboardInterrupt:
+            usedInputs.seek(0)
+            logFile.seek(0)
+            print("Fuzzing ended by user...\nFuzzer produced: "+str(len(usedInputs.readlines()))+ " inputs")
+            print("Fuzzer found "+str(len(logFile.readlines()))+ " interesting inputs   ")
             logFile.close()
             usedInputs.close()
             sortTraffic()
